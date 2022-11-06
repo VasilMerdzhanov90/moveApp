9f705e905207d622d791cdca1d3a0c908f4c155e
Vasil Merdzhanov <vasilmerdjanov1@gmail.com>
2022-11-06 18:53:53 +0200

    adding close to the search result

--- src/searchBar.js
+++ src/searchBar.js
@@ -2,34 +2,32 @@ import { listOfMoviesGenerator } from "./mainContentLoader.js";
 
 const searchForm = document.getElementById('search');
 searchForm.addEventListener('submit', searchMovie);
+
 async function searchMovie(e) {
     e.preventDefault()
+
     const formData = new FormData(e.target);
-    const value = formData.get('search')
+    const value = formData.get('search');
 
     const urlSearchByMovie = `https://api.themoviedb.org/3/search/movie?api_key=ca699f41df4cdcc6f6807864d5581587&language=en-US&query=${value}&page=1&include_adult=false`;
 
     try {
         if (value.length < 3) {
-            alert('Field must have at least 3 letters!')
-            return
+            alert('Field must have at least 3 letters!');
+            return;
         }
         const response = await fetch(urlSearchByMovie);
 
         if (response.ok == false) {
-            const error = await response.json()
+            const error = await response.json();
             alert(error.message)
-            return
+            return;
         }
         const data = await response.json();
-        generator(data.results)
-        // const foundMovies = document.createElement('div');
-        // foundMovies.className = 'found-movies';
-        // foundMovies.replaceChildren(...data.results.map(listOfMoviesGenerator))
-        // document.querySelector('div').appendChild(foundMovies)
-        // console.log(foundMovies)
+        generator(data.results);
+        e.target.reset()
     } catch (error) {
-        alert(error.message)
+        alert(error.message);
     }
 
 }
@@ -56,10 +54,18 @@ let exampleObject = {
 }
 
 function generator(data) {
+    const resultTab = document.querySelector('div[id="toggle"]');
+    resultTab.hidden = false;
+    searchForm.hidden = true;
     const foundMovies = document.createElement('div');
     foundMovies.className = 'found-movies';
-    foundMovies.replaceChildren(...data.map(listOfMoviesGenerator))
-    document.querySelector('div').appendChild(foundMovies)
-    console.log(foundMovies)
+    foundMovies.replaceChildren(...data.map(listOfMoviesGenerator));
+    document.querySelector('div').appendChild(foundMovies);
+    resultTab.addEventListener('click', () => closeSearch(foundMovies, resultTab));
+}
 
+function closeSearch(foundMovies, resultTab) {
+    foundMovies.remove();
+    resultTab.hidden = true;
+    searchForm.hidden = false;
 }
